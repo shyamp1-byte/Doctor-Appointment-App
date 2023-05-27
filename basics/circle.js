2617005b6a4c8ee793a86c6298dc5721ecb19c83
MEHER CHAITANYA <47631184+meherchaitanyabandaru@users.noreply.github.com>
2023-05-27 09:17:28 +0530

    first program written for shyam
    first program written for shyam

---
+++ basics/circle.js
@@ -0,0 +1,14 @@
+// Cirlce program to find the circumference and area in meters
+const pi=3.14;
+const radius=43;
+
+// below formula's are used to find the circumference and area
+let circumference=2*pi*radius;
+let area=pi*radius*radius;
+
+
+// printing the result
+console.log("circumference is :"+circumference+" meters");
+console.log("area is :"+ area+" meters");
+
+
