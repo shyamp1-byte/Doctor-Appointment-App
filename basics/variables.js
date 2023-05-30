a49d4136f345d73ecf673a4c0f1816a445d41207
shyamp1-byte <134669565+shyamp1-byte@users.noreply.github.com>
2023-05-30 08:18:45 +0530

    Variable changes 2
    2nd change

--- basics/variables.js
+++ basics/variables.js
@@ -1,5 +1,13 @@
 var number = 3; // collecting information through ear (variabe) and storing into brain (computer memory)
 //3 is person, seat number is memory address, theater is memory, fandango is movie ticket app
-console.log(number); //printing the information through mouth (console.log)
+//console.log(number); //printing the information through mouth (console.log)
 
 //Hospital: Patient information (Name, DOB, Gender, Weight)
+
+//let number1 = 10;
+//let number2 = 'shyam';
+//let result = number1 + number2;
+//console.log(typeof(number2));
+
+var number = 3; 
+console.log(typeof(number));
