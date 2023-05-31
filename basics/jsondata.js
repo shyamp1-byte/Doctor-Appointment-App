7a6635f18e1f9a6c035bcf9d36aae5c63c12a47e
MEHER CHAITANYA <47631184+meherchaitanyabandaru@users.noreply.github.com>
2023-05-31 09:22:39 +0530

    JSON
    JSON

---
+++ basics/jsondata.js
@@ -0,0 +1,16 @@
+// JSON Object (Javascript Object Notation)
+// XML , YAML , JSON    (Brothers)
+// we can use anything on our program . but JSON is popular
+
+// Please referer JSONCrack website
+const userInformation={
+    name:{firstName:"mehar",lastName:"Chaitanya"},
+    age:21,
+    address:{streetNo:01,city:"hyd",state:'telangana',country:"india",zipcode:500032},
+    agreed:true
+}
+
+
+// console.log(Object.keys(userInformation));
+
+console.log(userInformation);
---
+++ basics/simple_arrays.js
@@ -0,0 +1,16 @@
+// Homogenious  (Same Data type)  C / C++ / Java / GO
+let numbers=[10,20,30,50,70,80];  // Number array
+let floatnumbers=[10.4,23.3,55.2,77.6];  // Float Array
+let counries=["india","USA","China","Japan"]; // String Array
+
+// console.log(numbers,floatnumbers,counries);
+
+// Heterogenious (Different Data type)  Python / Javascript 
+let userInformation=['mehar chaitanya',29,'hyderabad',true,65.7]; // Different data type
+
+console.log(userInformation[0],",", typeof(userInformation[0]));
+console.log(userInformation[1],",", typeof(userInformation[1]));
+console.log(userInformation[2],",", typeof(userInformation[2]));
+console.log(userInformation[3],",", typeof(userInformation[3]));
+console.log(userInformation[4],",", typeof(userInformation[4]));
+console.log(userInformation,",",  typeof(userInformation));
