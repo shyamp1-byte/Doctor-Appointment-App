756236be6081826052771659302a5b920563ab2e
shyamp1-byte <134669565+shyamp1-byte@users.noreply.github.com>
2023-05-31 08:37:34 +0530

    update
    update

--- Playground/playground.js
+++ Playground/playground.js
@@ -1,1 +1,2 @@
 console.log("for experiments")
+
--- basics/variables.js
+++ basics/variables.js
@@ -2,4 +2,25 @@ var number = 3; // collecting information through ear (variabe) and storing into
 //3 is person, seat number is memory address, theater is memory, fandango is movie ticket app
 //console.log(number); //printing the information through mouth (console.log)
 
+<<<<<<< Updated upstream
 //Hospital: Patient information (Name, DOB, Gender, Weight)
+=======
+//Hospital: Patient information (Name, DOB, Gender, Weight)
+
+var name = 'Shyam'; //no need to use var, old syntax
+console.log(typeof name);
+
+// let city = 'Hyderabad';
+// city = "Mumbai";
+// console.log(city)
+
+var global = 10;
+
+function fun () {
+    var local = 5;
+    console.log(local);
+    console.log(global);
+}
+fun();
+console.log(global);
+>>>>>>> Stashed changes
