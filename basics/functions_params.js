61cfacb5509dfd5920818d8005552235c30fb0b4
MEHER CHAITANYA <47631184+meherchaitanyabandaru@users.noreply.github.com>
2023-06-01 09:11:01 +0530

    functions
    functions

---
+++ basics/functions_params.js
@@ -0,0 +1,36 @@
+// Function and method both are same.
+
+// 1 Python Programming 
+// def greeting(){
+//     print("Good Morning Python...!")
+// }
+
+
+// 2 Java Programming 
+// public void greeting(){
+//     System.out.println("Good Morning  Java");
+// }
+
+// Javascript  this is just for your understanding English Verbs (V1,V2,V3)
+
+
+
+// Step-1 Creating/ Defining  Function
+function add(number1,number2){
+    let total=number1+number2;
+    return total;
+}
+
+
+// Step-2 Calling the same function to run
+let result =add(10,20);
+console.log(result);
+// Advantage: you can re-use the function multiple times and in multiple locaitons / 
+// No need to type or recrete the similar program
+
+
+
+
+
+
+
