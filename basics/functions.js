907718c959bccb2646796c0e8b4f9516c2c0bc9b
MEHER CHAITANYA <47631184+meherchaitanyabandaru@users.noreply.github.com>
2023-06-01 08:55:34 +0530

    Create functions.js

---
+++ basics/functions.js
@@ -0,0 +1,24 @@
+// Function and method both are same.
+
+
+
+// 1 Python Programming 
+// def greeting(){
+//     print("Good Morning Python...!")
+// }
+
+
+// 2 Java Programming 
+// public void greeting(){
+//     System.out.println("Good Morning  Java");
+// }
+
+// Javascript
+function greeting(){
+    console.log("Good Morning....!");
+}
+
+let salary=10000; // 10000$ salary per month
+let expenses=4500; // 4500$ of expenses
+let remaining_balance=salary-expenses;
+console.log(remaining_balance);
