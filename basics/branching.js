c9487e85b6a2ef75a4c5f472e4d9eb7df702cc03
MEHER CHAITANYA <47631184+meherchaitanyabandaru@users.noreply.github.com>
2023-06-03 09:07:11 +0530

    Create branching.js

---
+++ basics/branching.js
@@ -0,0 +1,17 @@
+// Selecting something with condition
+
+let age=24;
+
+// Simple IF 
+// IF Else
+// Nested IF (If inside another IF CONDITION)
+
+// RESTURANT  (and &&)  (or ||)     (Not !)
+if(age<=10){
+    console.log("Kid - Give him some milk bread");    
+}
+else if(age<=18){
+    console.log("Adult - Give him some Pizza");
+}
+
+console.log(age);
