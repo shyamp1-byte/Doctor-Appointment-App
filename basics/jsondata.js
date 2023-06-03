c3fefa5c6269faa4d07e9d8b2c97eccc8d8e485d
shyamp1-byte <134669565+shyamp1-byte@users.noreply.github.com>
2023-06-03 09:08:42 +0530

    update
    update

--- basics/jsondata.js
+++ basics/jsondata.js
@@ -1,10 +1,11 @@
 // JSON Object (Javascript Object Notation)
+// JSON used to store data; not in database permamently
 // XML , YAML , JSON    (Brothers)
 // we can use anything on our program . but JSON is popular
 
 // Please referer JSONCrack website
 const userInformation={
-    name:{firstName:"mehar",lastName:"Chaitanya"},
+    name:{firstName:"Shyam",lastName:"Pedibhotla"},
     age:21,
     address:{streetNo:01,city:"hyd",state:'telangana',country:"india",zipcode:500032},
     agreed:true
