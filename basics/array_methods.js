a198ee66bb027d70ed7dc5cd53c00a088c1aab89
shyamp1-byte <134669565+shyamp1-byte@users.noreply.github.com>
2023-06-06 08:48:33 +0530

    update
    update

---
+++ basics/array_methods.js
@@ -0,0 +1,26 @@
+let userInformation=['mehar chaitanya',29,'hyderabad',true,65.7]; 
+
+console.log(userInformation,",",  typeof(userInformation));
+
+console.log("------------------------------")
+console.log("Printing length of the array: ")
+console.log(userInformation.length);
+console.log("------------------------------")
+
+console.log("-----------------------------")
+console.log("Printing 4th index of array: ")
+console.log(userInformation[4])
+console.log(userInformation[userInformation.length-1])
+console.log("-----------------------------")
+
+
+
+let fruits = ["Banana", "Orange", "Apple", "Mango", "Apricot"]
+console.log("---------------------------------")
+console.log("Printing length of fruits array: ")
+console.log(fruits.length);
+console.log("---------------------------------")
+console.log(fruits)
+console.log(fruits.sort())
+console.log(fruits.pop())
+console.log(fruits.push())
--- basics/math_problems/sphere.js
+++ basics/math_problems/sphere.js
@@ -1,6 +1,6 @@
 //v = 4/3pi(r**3)
 
-<<<<<<< HEAD
+
 let volume = null;
 const pi = 3.14
 let radius = 1;
@@ -9,7 +9,7 @@ for(radius=1;radius<=10;radius++) {
     volume = 4/3*(pi)*(radius**3)
     console.log("The Radius is " +radius, "and The Volume is " +volume)
 }
-=======
+
 // funciton will take "radius" as the input [and] prints "volume" as the output
 function findSphereRadius(radius){
     const pi = 3.14; 
@@ -22,4 +22,3 @@ for(let radius=1;radius<=100;radius++) {
     console.log("The Radius is "+ radius, " Volume is : "+findSphereRadius(radius));
 }
 
->>>>>>> 451a125b5da662ddd40aa08f985f08e4df3ba770
