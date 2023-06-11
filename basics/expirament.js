0a7cc268165cf5c0cded5d02422d31e22b73775e
shyamp1-byte <134669565+shyamp1-byte@users.noreply.github.com>
2023-06-11 17:30:26 +0530

    patient info update
    update

---
+++ basics/expirament.js
@@ -0,0 +1,12 @@
+//Print 1-24 numbers, take as input. Do greeting.
+
+num = [1,24];
+consoleInput(prompt("Give me number 1 through 24"));
+
+if (num<8) {
+    console.log("It is early, wake up and study")
+} else if (num<16) {
+    console.log("Eat food")
+} else {
+    console.log("Go to sleep")
+}
