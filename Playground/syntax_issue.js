fc5d01f79843a6c5d2c7457080f718898c72714b
MEHER CHAITANYA <47631184+meherchaitanyabandaru@users.noreply.github.com>
2023-06-12 12:25:06 +0530

    Create syntax_issue.js

---
+++ Playground/syntax_issue.js
@@ -0,0 +1,16 @@
+let number=10;
+console.log("number is "+ number + " and data type us : "+ typeof(number));
+console.log("number is "+ number.toString() + " and data type us : "+ typeof(number.toString));
+
+
+
+// Concept is important. No need to remember the syntax of the language
+
+// number.toString() in JAVASCRIPT
+// String.valueOf(number); in JAVA
+// string to_string (int num); in C++
+// str(number)  in PYTHON
+// result := strconv.Itoa(number) in GO-LANGUAGE
+// char snum[5];     in C-LANGUAGE
+
+
