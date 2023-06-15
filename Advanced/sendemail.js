b93402480ada58b56b6ef0c13c029ba947662f3c
shyamp1-byte <134669565+shyamp1-byte@users.noreply.github.com>
2023-06-15 16:57:45 +0530

    update
    update

--- Advanced/sendemail.js
+++ Advanced/sendemail.js
@@ -1,24 +1,55 @@
-var nodemailer = require('nodemailer');
+// var nodemailer = require('nodemailer');
 
-var transporter = nodemailer.createTransport({
-  service: 'gmail',
+// var transporter = nodemailer.createTransport({
+//   service: 'yahoo',
+//   auth: {
+//     user: 'shyampedibhotla1@yahoo.com',
+//     pass: 'shyamp_125'
+//   }
+// });
+
+// var mailOptions = {
+//   from: 'shyampedibhotla1@yahoo.com',
+//   to: 'mehermc360@gmail.com',
+//   subject: 'Sending Email using Node.js',
+//   text: 'That was easy!'
+// };
+
+// transporter.sendMail(mailOptions, function(error, info){
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log('Email sent: ' + info.response);
+//   }
+// });
+//Port = 587, mention TLS = true, SMTP address = smtp.yahoo.com
+
+const nodemailer = require('nodemailer');
+
+// Create a transporter using your Yahoo SMTP credentials
+const transporter = nodemailer.createTransport({
+  host: 'smtp.mail.yahoo.com',
+  port: 465,
+  secure: true, // TLS enabled
   auth: {
-    user: 'shyamie123@gmail.com',
-    pass: 'Lil_Buddha774749'
-  }
+    user: 'ruthvik1@yahoo.com',
+    pass: 'Truckmasti123',
+  },
 });
 
-var mailOptions = {
-  from: 'shyamie123@gmail.com',
+// Email message options
+const mailOptions = {
+  from: 'ruthvik1@yahoo.com',
   to: 'mehermc360@gmail.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
+  subject: 'Hello from Yahoo',
+  text: 'This is a test email sent from Yahoo to Gmail!',
 };
 
-transporter.sendMail(mailOptions, function(error, info){
+// Send the email
+transporter.sendMail(mailOptions, (error, info) => {
   if (error) {
-    console.log(error);
+    console.log('Error occurred:', error);
   } else {
-    console.log('Email sent: ' + info.response);
+    console.log('Email sent:', info.response);
   }
 });
