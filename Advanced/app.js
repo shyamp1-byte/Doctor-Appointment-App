334a22ac365d1dea1bde70e068a84e088445a4c7
shyamp1-byte <134669565+shyamp1-byte@users.noreply.github.com>
2023-06-16 17:01:44 +0530

    update
    uodate

--- Advanced/app.js
+++ Advanced/app.js
@@ -1,37 +1,11 @@
-const chalk = require('chalk');
-const {add,sub,mul,div}=require('./utils/arthematic_operations');
-const geometry=require("./geometry_operations");
+const express = require('express')
+const app = express()
+const port = 3000
 
-function withdrawBankBalaance(availableBalance,needtoWithdraw){
-    try {
-        let result=null;
-        if(availableBalance>needtoWithdraw){
-            result=availableBalance-needtoWithdraw;
-            console.log(chalk.bgGreen("Here is the amount of money withdrawn: "+result+"  INR"));
-            return result;
-        }
-        else{
-            throw new TypeError("Type error occurred");
-        }
-        
-    } catch (error) {
-        console.log(chalk.bgRed("Unable to withdraw your cash from this ATM at this movement. Please check your balance"))
-        return "Unable to withdraw your cash from this ATM at this movement. Please check your balance";
-    }
-    finally{  // Optional (no need to define)
-        // Always run this statement
+app.get('/', (req, res) => {
+  res.send('Hello Good Morning!')
+})
 
-        console.log(chalk.bgBlue("Thank you for Visiting ICICI ATM - Have a good day...! "));
-    }
-    
-}
-
-withdrawBankBalaance(4,7);
-
-console.log(chalk.bgBlue(add(5,6)));
-console.log(chalk.bgBlue(sub(5,6)));
-console.log(chalk.bgRedBright(mul(5,6)));
-console.log(chalk.bgGreen((div(5,6))));
-
-console.log(geometry.calculateCircleInfo(5));
-console.log(geometry.calculateSqareInfo(64));
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
