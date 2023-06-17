e5b6f31c2db8eb671cab1c72d199f9381486641c
MEHER CHAITANYA <47631184+meherchaitanyabandaru@users.noreply.github.com>
2023-06-17 16:08:07 +0530

    updated
    updated

--- Advanced/app.js
+++ Advanced/app.js
@@ -6,6 +6,35 @@ app.get('/', (req, res) => {
   res.send('Hello Good Morning!')
 })
 
+app.get('/USERS', (req, res) => {
+  const users=[
+    {username:"mehar",city:"Hyd"},
+    {username:"shyam",city:"DEL"},
+    {username:"ram",city:"BAN"},
+    {username:"naveen",city:"NOI"},
+    {username:"john",city:"VSK"},
+    {username:"chaitanya",city:"CHE"}
+  ]
+  res.send(users);
+})
+
+app.get('/SIGN-UP', (req, res) => {
+  res.send('im from SIGN-UP');
+})
+
+
+app.get('/FORGOT', (req, res) => {
+  res.send('im from FORGOT PASSWORD ');
+})
+
+app.get('*', (req, res) => {
+  res.send('404 - No data found for your request');
+})
+
+app.get('/HELP', (req, res) => {
+  res.send('CALL US 9898987978979');
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
