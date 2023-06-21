4b229841bff3c1df2ad17cccf0dd5a40d59eba34
shyamp1-byte <134669565+shyamp1-byte@users.noreply.github.com>
2023-06-21 19:39:19 +0530

    update
    update

--- adv-shyam/Schema/userSchema.js
+++ adv-shyam/Schema/userSchema.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require('mongoose');
-const {Schema} = mongoose;
-const validator = require('validator');
-
-
-const userSchema = new Schema({
-  fullName: {
-    firstName: {
-=======
 const mongoose = require ("mongoose");
 const { Schema } = mongoose;
 const validator = require('validator');
@@ -38,19 +28,10 @@ const userSchema = new Schema({
       trim: true,
     },
     gender: {
->>>>>>> 0f6af0ac25ec84afb14cfe26e921cee7a3481983
       type: String,
       required: true,
       trim: true,
     },
-<<<<<<< HEAD
-    middleName: {
-      type: String,
-      required: false,
-      trim: true,
-    },
-    lastName: {
-=======
     email: {
       type: String,
       required: true,
@@ -63,44 +44,10 @@ const userSchema = new Schema({
       },
     },
     phoneNumber: {
->>>>>>> 0f6af0ac25ec84afb14cfe26e921cee7a3481983
       type: String,
       required: true,
       trim: true,
     },
-<<<<<<< HEAD
-  },
-  dateOfBirth: {
-    type: Date,
-    min: '1950-01-01',
-    max: '2022-12-31',
-    required: true,
-    trim: true,
-  },
-  gender: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error('Email is invalid');
-      }
-    },
-  },
-  phoneNumber: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  address: {
-    doorNo: {
-=======
     address: {
       doorNo: {
         type: String,
@@ -145,60 +92,11 @@ const userSchema = new Schema({
       },
     },
     UID: {
->>>>>>> 0f6af0ac25ec84afb14cfe26e921cee7a3481983
       type: String,
       required: true,
       trim: true,
     },
-<<<<<<< HEAD
-    streetName: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    city: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    state: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    country: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    zipcode: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 7,
-    trim: true,
-    validate(value) {
-      if (value.toLowerCase().includes('password')) {
-        throw new Error('Password cannot contain "password"');
-      }
-    },
-  },
-  UID: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-}, {timestamps: {createdAt: 'created_at', updatedAt: 'updated_at'}});
-
-module.exports = userSchema;
-=======
   }, {timestamps: {createdAt: 'created_at', updatedAt: 'updated_at'}});
 
  
   module.exports = userSchema;
->>>>>>> 0f6af0ac25ec84afb14cfe26e921cee7a3481983
