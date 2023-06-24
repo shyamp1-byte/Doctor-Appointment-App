3f01fc8b92484618dac1a1467ac5cd95422bd063
shyamp1-byte <134669565+shyamp1-byte@users.noreply.github.com>
2023-06-24 12:03:19 +0530

    update
    update

--- adv-shyam/Routers/userRouter.js
+++ adv-shyam/Routers/userRouter.js
@@ -6,4 +6,7 @@ router.route('/')
 .post(userController.createNewUser)
 .get(userController.getAllUsers);
 
+router.route('/:email')
+.get(userController.getUsersByEmail);
+
 module.exports = router;
