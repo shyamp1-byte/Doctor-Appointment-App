e7a234e817b60a4d3923e5281fc35d71b897771d
MEHER CHAITANYA <47631184+meherchaitanyabandaru@users.noreply.github.com>
2023-07-05 11:57:21 +0530

    Update appointmentController.js

--- adv-shyam/Controllers/appointmentController.js
+++ adv-shyam/Controllers/appointmentController.js
@@ -17,7 +17,7 @@ req.body.patientName=patientInfo[0].fullName.firstName;
 
 
 // Check doctor ID is valid or not ::: START
-const doctorID=await docModel.find({"doctorId":req.body.doctorId});
+const doctorID=await docModel.find({"doctorId":req.body.doctorID});
 if(doctorID[0].DID!==req.body.doctorID){
   return res.status(403).send('This doctor ID is not registred / Invalid');
 }
