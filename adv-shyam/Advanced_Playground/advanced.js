38cfa1e3f7dff814878a9023049d260aad0af2cd
shyamp1-byte <134669565+shyamp1-byte@users.noreply.github.com>
2023-07-07 12:27:55 +0530

    update
    update

--- adv-shyam/Advanced_Playground/advanced.js
+++ adv-shyam/Advanced_Playground/advanced.js
@@ -1,4 +1,36 @@
-const data=require('./data.json');
+const data = require("./data.json");
 
+//console.log(data);
+//console.log(typeof(data)
+function displayUsers() {
+  for (let i = 0; i < data.users.length; i++) {
+    let firstName = data.users[i].firstName;
+    let ssn = data.users[i].ssn;
+    let phone = data.users[i].phone;
+    let result = {
+      firstName,
+      ssn,
+      phone,
+    };
+    console.log(i + 1, ":", result);
+  }
+}
+// displayUsers();
 
-console.log(data);
+function searchUsers(firstName) {
+  for (let i = 0; i < data.users.length; i++) {
+    if (firstName === data.users[i].firstName) {
+      console.log("Match Found in the", i + 1, "record");
+      console.log(
+        "Information in",
+        i + 1,
+        "record:" + JSON.stringify(data.users[i])
+      );
+    }
+  }
+}
+searchUsers("Macy");
+
+for (let i = 0; i < data.users.length; i++) {
+    let phone = data.users[i].phone;
+    if (phone  data.users[i].phone) {
